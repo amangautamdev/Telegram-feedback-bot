eca6c57e5d4f0aede1564cc65e4e2c2f8f32efea
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-04-12 08:54:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 	API_HASH = os.environ.get("API_HASH", "9f2f1d81fd2af2b21a700fa6681215b1")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "7112282983:AAFS1ji7qHjB7vozFwK8IN5KGfqWKXK_bMA")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	BOT_OWNER = int(os.environ.get("5631563685"))
+	BOT_OWNER = "5631563685"
 	SESSION_STRING = environ.get("SESSION_STRING", None)
 	OWNER = list(map(int, getenv("OWNER").split())) # ain karanna epa hehe
 	MONGODB_URI = os.environ.get("MONGODB_URI")
